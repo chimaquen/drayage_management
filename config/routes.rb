805ee1fdf625a1879645d51175d574dea1fa799d
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2021-06-11 23:21:51 +0900

    Basic Layout with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
   }
 
-  get 'home/index'
-  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
