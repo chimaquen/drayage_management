cc011864ff996af735c71c3f8fcee6236a36da1a
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2021-06-13 00:00:29 +0900

    Edit, Delete, Show

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -11,8 +11,26 @@ class SchedulesController < ApplicationController
     Schedule.create!(pickup_date: schedule_params[:pickup_date], delivery_date: schedule_params[:delivery_date], allowed_time: schedule_params[:allowed_time], cy: schedule_params[:cy], warehouse: schedule_params[:warehouse], bill_number: schedule_params[:bill_number], container: schedule_params[:container], size: schedule_params[:size], axis: schedule_params[:axis], transporter: schedule_params[:transporter], division: schedule_params[:division], customer: schedule_params[:customer], reference_number: schedule_params[:reference_number], trade_type: schedule_params[:trade_type])
   end
 
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def edit
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+    schedule = Schedule.find(params[:id])
+    schedule.update(schedule_params)
+  end
+
+  def destroy
+    schedule = Schedule.find(params[:id])
+    schedule.destroy
+  end
+
   private
   def schedule_params
-    params.require(:schedule).permit(:pickup_date, :delivery_date, :allowed_time, :cy, :warehouse, :bill_number, :container, :size, :axis, :transporter, :division, :customer, :reference, :trade_type)
+    params.require(:schedule).permit(:pickup_date, :delivery_date, :allowed_time, :cy, :warehouse, :bill_number, :container, :size, :axis, :transporter, :division, :customer, :reference_number, :trade_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'schedules#index'
   get '/schedules/new', to: "schedules#new"
   post '/schedules', to: "schedules#create"
+  get '/schedules/:id', to: "schedules#show"
+  get '/schedules/:id/edit', to: "schedules#edit"
+  patch '/schedules/:id', to: "schedules#update"
+  delete '/schedules/:id', to: 'schedules#destroy'
   devise_for :users, controllers: {
       sessions:      'users/sessions',
       passwords:     'users/passwords',
